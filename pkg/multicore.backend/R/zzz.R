d1228604d118a688537c4ed692127cd229ff5287
kaneplusplus (Mr ) <kaneplusplus@gmail.com>
2011-04-14 01:46:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/background@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/multicore.backend/R/zzz.R
@@ -0,0 +1,64 @@
+
+multicore.process <- setRefClass("multicore.process",
+  contains="process.interface",
+  fields = list(pj="ANY"),
+
+  methods = list(
+
+    initialize=function() {
+      callSuper()
+    },
+
+    task=function(evalExpr, evalEnv=parent.frame()) {
+      .self$check.killed()
+      if (evaluated)
+        stop("This background object has already evaluated an expression")
+      env <<- evalEnv
+      expr <<- evalExpr
+      has.task <<- TRUE
+      pj <<- background:::mcpar(expr, env)
+    },
+
+    value=function() {
+      .self$check.killed()
+      if (!evaluated) {
+        v <- collect(pj)
+        if (length(v) == 0) {
+          val <<- NULL
+        } else {
+          val <<- v[[1]]
+        }
+        evaluated <<- TRUE
+      }
+      val
+    },
+
+    done=function() {
+      .self$check.killed()
+      ret <- FALSE
+      if (evaluated) {
+        ret <- TRUE
+      } else {
+        v <- collect(pj, wait=FALSE)
+        if( !is.null(v) ) {
+          if (length(v) == 0) {
+            val <<- NULL
+          } else {
+            val <<- v[[1]]
+          }
+          evaluated <<- TRUE
+        }
+      }
+      ret
+    },
+
+    kill=function() {
+      .self$check.killed()
+      multicore::kill(pj)
+      killed <<- TRUE
+      TRUE
+    }
+  
+  )
+)
+assign("multicore.process", multicore.process, .GlobalEnv)
