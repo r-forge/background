23036cfe619556ced2d2531de5cc968baac1ca36
kaneplusplus (Mr ) <kaneplusplus@gmail.com>
2012-05-20 03:52:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/background@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/background/R/background.r
+++ pkg/background/R/background.r
@@ -1,8 +1,4 @@
 
-require(methods)
-require(utils)
-
-
 process.interface <- setRefClass("process.interface",
   fields = list(.evaluated="logical", .has.task="logical", .expr="ANY", 
     .env="environment", .val="ANY", .killed="logical"),
@@ -37,7 +33,7 @@ process.interface <- setRefClass("process.interface",
 
   )
 )
-assign("process.interface", process.interface, .GlobalEnv)
+#assign("process.interface", process.interface, .GlobalEnv)
 
 sequential.process <- setRefClass("sequential.process",
   contains="process.interface",
@@ -82,5 +78,58 @@ sequential.process <- setRefClass("sequential.process",
 
   )
 )
-assign("sequential.process", sequential.process, .GlobalEnv)
+
+bg <- function(expr, par.gen=options()$background.backend) {
+  if (is.null(options()$background.backend)) {
+    warning("No backend specified, sequential will be used")
+    options(background.backend="sequential.process")
+    par.gen <- "sequential.process"
+  }
+  process <- eval(parse(text=paste(par.gen, '$new()', sep='')))
+  process$task(substitute(expr))
+  process
+}
+
+fg <- function( process ) {
+  process$value()
+}
+
+done <- function( process ) {
+  process$done()
+}
+
+kill <- function( process ) {
+  process$kill()
+}
+
+killed <- function( process ) {
+  process$killed()
+}
+
+backend <- function( backendName=NULL ) {
+  if (is.null(backendName)) {
+    if (is.null(options()$background.backend)) {
+      stop("No backend has been specified")
+    } else {
+      return(options()$background.backend)
+    }
+  }
+  # For right now I can only check to see if the backendName exists.
+  # It would be nice to see if it inherits from process.interface.
+  if (!exists(backendName)) {
+    stop(paste("The reference class", backendName, "could not be found"))
+  } else {
+    options(background.backend=backendName)
+  }
+  return(TRUE)
+}
+
+superclasses <- function(x, what) {
+  if (class(x) != "refObjectGenerator") {
+    stop("The object passed is not a reference class")
+  }
+  meth <- x$methods()
+  meth <- meth[grep("#", meth)]
+  unique(matrix(unlist(strsplit(meth, "#")), ncol=2, byrow=TRUE)[,2])
+}
 
